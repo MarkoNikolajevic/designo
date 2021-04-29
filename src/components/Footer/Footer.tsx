c1f3a76fdcbf66a7ab53aae48a8aa96d4b9dedb4
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-04-29 22:53:35 +0200

    style footer for medium and large viewport

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,29 +6,34 @@ import SocialList from './SocialList';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href='/'>
-        <a className={styles.footer__logo}>
-          <img src='/assets/shared/desktop/logo-light.png' alt='Designo logo' />
-        </a>
-      </Link>
-      <ul className={styles.footer__menu}>
-        <li className={styles.footer__menu__item}>
-          <ActiveLink activeClassName='active' href='/about'>
-            <a>Our Company</a>
-          </ActiveLink>
-        </li>
-        <li className={styles.footer__menu__item}>
-          <ActiveLink activeClassName='active' href='/locations'>
-            <a>Locations</a>
-          </ActiveLink>
-        </li>
-        <li className={styles.footer__menu__item}>
-          <ActiveLink activeClassName='active' href='/contact'>
-            <a>Contact</a>
-          </ActiveLink>
-        </li>
-      </ul>
-      <div className={styles.footer__info_wrapper}>
+      <div className={`${styles.footer__firstRow} container`}>
+        <Link href='/'>
+          <a className={styles.footer__logo}>
+            <img
+              src='/assets/shared/desktop/logo-light.png'
+              alt='Designo logo'
+            />
+          </a>
+        </Link>
+        <ul className={styles.footer__menu}>
+          <li className={styles.footer__menu__item}>
+            <ActiveLink activeClassName='active' href='/about'>
+              <a>Our Company</a>
+            </ActiveLink>
+          </li>
+          <li className={styles.footer__menu__item}>
+            <ActiveLink activeClassName='active' href='/locations'>
+              <a>Locations</a>
+            </ActiveLink>
+          </li>
+          <li className={styles.footer__menu__item}>
+            <ActiveLink activeClassName='active' href='/contact'>
+              <a>Contact</a>
+            </ActiveLink>
+          </li>
+        </ul>
+      </div>
+      <div className={`${styles.footer__secondRow} container`}>
         <div>
           <p className={styles.footer__info}>
             <strong>Designo Central Office</strong>
@@ -43,8 +48,8 @@ const Footer = () => {
           <p className={styles.footer__info}>P : +1 253-863-8967</p>
           <p className={styles.footer__info}>M : contact@designo.co</p>
         </div>
+        <SocialList />
       </div>
-      <SocialList />
     </footer>
   );
 };
