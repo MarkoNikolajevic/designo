232ef4614616f0431508345328fa1e6fab5c9a26
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-05-04 22:59:27 +0200

    finish graphic design page

--- src/pages/graphic-design.tsx
+++ src/pages/graphic-design.tsx
@@ -1,4 +1,8 @@
+import styles from '../styles/GraphicDesign.module.scss';
 import InnerPageHeader from '../components/InnerPageHeader/InnerPageHeader';
+import ProjectCard from '../components/ProjectCard/ProjectCard';
+import ProjectCta from '../components/ProjectCta/ProjectCta';
+import graphicDesignProjects from '../utils/graphicDesignProjects';
 
 export default function GraphicDesign() {
   return (
@@ -7,6 +11,30 @@ export default function GraphicDesign() {
         title='Graphic Design'
         paragraph='We deliver eye-catching branding materials that are tailored to meet your business objectives.'
       />
+      <section>
+        <div className={`${styles.grid} container`}>
+          {graphicDesignProjects.map((project) => {
+            return (
+              <ProjectCard
+                key={project.id}
+                img={project.img}
+                title={project.title}
+                description={project.description}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <section>
+        <div className={`${styles.cta__grid} container`}>
+          <ProjectCta id='app' link='/app-design' title='App design' />
+          <ProjectCta
+            id='web'
+            link='/web-design'
+            title='Web design'
+          />
+        </div>
+      </section>
     </>
   );
 }
--- src/utils/graphicDesignProjects.ts
+++ src/utils/graphicDesignProjects.ts
@@ -2,19 +2,19 @@ const graphicDesignProjects = [
   {
     id: 'tim_brown',
     title: 'Tim Brown',
-    img: '/assets/web-design/desktop/image-change.jpg',
-    description: `A book cover designed for Tim Brown’s new release, ‘Change’`,
+    img: '/assets/graphic-design/desktop/image-change.jpg',
+    description: 'A book cover designed for Tim Brown’s new release, ‘Change’',
   },
   {
     id: 'boxed_water',
     title: 'Boxed Water',
-    img: '/assets/web-design/desktop/image-boxed-water.jpg',
+    img: '/assets/graphic-design/desktop/image-boxed-water.jpg',
     description: 'A simple packaging concept made for Boxed Water',
   },
   {
     id: 'science!',
-    titles: 'Science!',
-    img: '/assets/web-design/desktop/image-science.jpg',
+    title: 'Science!',
+    img: '/assets/graphic-design/desktop/image-science.jpg',
     description: 'A poster made in collaboration with the Federal Art Project',
   },
 ];
