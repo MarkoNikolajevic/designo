1f2585e76681f0e19b5d2f2cd740559d1e6aaebf
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-05-04 22:50:40 +0200

    finish web design page

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -3,9 +3,11 @@ import styles from './ProjectCard.module.scss';
 const ProjectCard = (props: any) => {
   return (
     <div className={styles.card}>
-      <img src={props.img} alt={props.id} />
-      <h3 className={styles.card__title}>{props.id}</h3>
-      <p className={styles.card__description}>{props.description}</p>
+      <img src={props.img} alt={props.id} className={styles.card__img} />
+      <div className={styles.card__text}>
+        <h3 className={styles.card__title}>{props.title}</h3>
+        <p className={styles.card__description}>{props.description}</p>
+      </div>
     </div>
   );
 };
