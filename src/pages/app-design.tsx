091cfd87c896e30bb93cd803e020797f62c3f876
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-05-04 23:04:45 +0200

    finish app design page

--- src/pages/app-design.tsx
+++ src/pages/app-design.tsx
@@ -1,4 +1,8 @@
+import styles from '../styles/AppDesign.module.scss';
 import InnerPageHeader from '../components/InnerPageHeader/InnerPageHeader';
+import ProjectCard from '../components/ProjectCard/ProjectCard';
+import ProjectCta from '../components/ProjectCta/ProjectCta';
+import appDesignProjects from '../utils/appDesignProjects';
 
 export default function AppDesign() {
   return (
@@ -7,6 +11,30 @@ export default function AppDesign() {
         title='App Design'
         paragraph='Our mobile designs bring intuitive digital solutions to your customers right at their fingertips.'
       />
+      <section>
+        <div className={`${styles.grid} container`}>
+          {appDesignProjects.map((project) => {
+            return (
+              <ProjectCard
+                key={project.id}
+                img={project.img}
+                title={project.title}
+                description={project.description}
+              />
+            );
+          })}
+        </div>
+      </section>
+      <section>
+        <div className={`${styles.cta__grid} container`}>
+          <ProjectCta id='web' link='/web-design' title='Web design' />
+          <ProjectCta
+            id='graphic'
+            link='/graphic-design'
+            title='Graphic design'
+          />
+        </div>
+      </section>
     </>
   );
 }
--- src/utils/appDesignProjects.ts
+++ src/utils/appDesignProjects.ts
@@ -2,34 +2,34 @@ const appDesignProjects = [
   {
     id: 'airfilter',
     title: 'airfilter',
-    img: '/assets/web-design/desktop/image-airfilter.jpg',
+    img: '/assets/app-design/desktop/image-airfilter.jpg',
     description:
       'Solving the problem of poor indoor air quality by filtering the air',
   },
   {
     id: 'eyecam',
     title: 'eyecam',
-    img: '/assets/web-design/desktop/image-eyecam.jpg',
+    img: '/assets/app-design/desktop/image-eyecam.jpg',
     description:
       'Product that lets you edit your favorite photos and videos at any time',
   },
   {
     id: 'faceit',
     title: 'faceit',
-    img: '/assets/web-design/desktop/image-faceit.jpg',
+    img: '/assets/app-design/desktop/image-faceit.jpg',
     description:
       'Get to meet your favorite internet superstar with the faceit app',
   },
   {
     id: 'todo',
     title: 'todo',
-    img: '/assets/web-design/desktop/image-todo.jpg',
+    img: '/assets/app-design/desktop/image-todo.jpg',
     description: 'A todo app that features cloud sync with light and dark mode',
   },
   {
     id: 'loopstudios',
     title: 'loopstudios',
-    img: '/assets/web-design/desktop/image-loopstudios.jpg',
+    img: '/assets/app-design/desktop/image-loopstudios.jpg',
     description: 'A VR experience app made for Loopstudios',
   },
 ];
