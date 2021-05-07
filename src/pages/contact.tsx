2c3c413f5885631471d202cb3359d43414f7fbac
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-05-07 21:55:51 +0200

    add locations on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,29 @@
+import styles from '../styles/Contact.module.scss';
+import LocationCard from '../components/LocationCard/LocationCard';
+
 export default function Contact() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, magni.
-      Alias nihil eius ipsum sequi consequuntur libero nesciunt temporibus?
-    </div>
+    <>
+      <section className={styles.contact__grid}>
+        <LocationCard
+          link='/locations/#canada'
+          location='Canada'
+          locationImg='/assets/shared/desktop/illustration-canada.svg'
+          imgBgClass='to_top_bg'
+        />
+        <LocationCard
+          link='/locations/#australia'
+          location='Australia'
+          locationImg='/assets/shared/desktop/illustration-australia.svg'
+          imgBgClass='to_left_bg'
+        />
+        <LocationCard
+          link='/locations/#united-kingdom'
+          location='United Kingdom'
+          locationImg='/assets/shared/desktop/illustration-united-kingdom.svg'
+          imgBgClass='to_bottom_bg'
+        />
+      </section>
+    </>
   );
 }
