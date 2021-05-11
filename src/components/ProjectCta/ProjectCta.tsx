0c4a1d03e95db2aa42f996202eac995452b452f6
Marko Nikolajevic <mrknikolajevic@gmail.com>
2021-05-11 23:56:31 +0200

    fix project cta link

--- src/components/ProjectCta/ProjectCta.tsx
+++ src/components/ProjectCta/ProjectCta.tsx
@@ -5,12 +5,18 @@ const Button = (props: { title: string; link: string; id: string }) => {
 
   return (
     <div className={styles.cta} id={props.id}>
-      <div className={styles.cta__overlay}>
-        <h2>{props.title}</h2>
-        <Link href={props.link}>
-          <a>View projects</a>
-        </Link>
-      </div>
+      <Link href={props.link}>
+        <div className={styles.cta__overlay}>
+          <h2>{props.title}</h2>
+          <a>
+            View projects
+            <img
+              src='/assets/shared/desktop/icon-right-arrow.svg'
+              alt='arrow right icon'
+            />
+          </a>
+        </div>
+      </Link>
     </div>
   );
 };
