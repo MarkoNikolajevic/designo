d59d125f6a38b6b8779cfccf317b7cc2e6a63cae
Marko Nikolajevic <mrknikolajevic@gmail.com>
2024-06-08 23:27:06 +0200

    add colors and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur vero iste hic suscipit magni, quibusdam sapiente laudantium.</div>
-    </main>    
+    <main className='bg-grey-900'>
+      <div className='text-grey-100'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+        vero iste hic suscipit magni, quibusdam sapiente laudantium.
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,20 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      current: 'currentColor',
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      grey: {
+        100: '#F1F3F5',
+        900: '#333136',
+      },
+      peach: {
+        100: '#FFAD9B',
+        500: '#E7816B',
+      },
+    },
     extend: {}
   },
   plugins: [],
