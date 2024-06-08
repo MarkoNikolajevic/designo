623c3c43db28c37d09ce57c89e8af399f3ae0888
Marko Nikolajevic <mrknikolajevic@gmail.com>
2024-06-08 22:59:36 +0200

    first commit new version

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur vero iste hic suscipit magni, quibusdam sapiente laudantium.</div>
+    </main>    
+  );
+}
